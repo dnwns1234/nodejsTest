d35ba3daea69b76d64a43e8e6613d03df41894e2
dnwns1234 <83625417+dnwns1234@users.noreply.github.com>
2023-07-29 01:08:18 +0900

    first commit
    nodejs 테스트

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/sound/:name', function (req, res) {
+    const {name} = req.params
+    console.log(req.params)
+
+    if(name == "dog"){
+        res.json({'sound':'멍멍'})
+    }
+    else if(name == "cat"){
+        res.json({'sound':'야옹'})
+    }
+    else if(name == "pig"){
+        res.json({'sound':'꿀꿀'})
+    }
+    else{
+        res.json({'sound':'몰루'})
+    }
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
